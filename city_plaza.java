45964aebb2d9ad7eddc597af75d58dd2bd84216a
sawan <sawankumar00121016@gmail.com>
2025-01-30 07:30:13 +0530

    Add files via upload

---
+++ city_plaza.java
@@ -0,0 +1,25 @@
+public class city_plaza {
+//    if statement how used different ways
+    public static int max(int a,int b){
+        return (a>b) ? a:b; //this is if and else statement
+    }
+    public static void leap() {
+        for (int year = 1; year <= 50; year++) {
+            int i = (year % 4 == 0) ? year : year;
+            System.out.println(i);
+            if (year % 4 == 0){
+                System.out.println(year);
+            }
+        }
+    }
+    public static void main(String[] args){
+//        while(1){ //this condition parduces Error
+//        System.out.println("hello");
+//    }
+        System.out.println(max(6,5));
+
+            leap();
+//        leap year print 1 to 50
+
+        }
+}
